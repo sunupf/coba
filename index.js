f5f39baa6710ca242e894557042a5aac78dae650
kokok <sunupf@gmail.com>
2015-09-06 12:42:27 +0700

    trying loging

---
+++ index.js
@@ -0,0 +1,11 @@
+var loop = require('./lib/test');
+var helper = require('./lib/helper');
+var binding = require('./lib/binding');
+var _ = require('./lib/binding');
+
+module.exports = {
+  'loop': loop,
+  'helper': helper,
+  'binding': binding,
+  'lodash': _
+}
---
+++ template/log/data.js
@@ -0,0 +1,37 @@
+var testingResults = {
+  'title' : "Registration Test",
+  'log' : {
+    'firefox' : {
+      'null' : [
+        {
+          'input' : 'lorem ipsum dolor sit amet',
+          'output' : 'data is required',
+          'expected' : 'Success',
+          'load' : 120,
+          'execution' : 420
+        },
+        {
+          'input' : 'lorem ipsum dolor sit amet',
+          'output' : 'Success',
+          'expected' : 'Success',
+          'load' : 350,
+          'execution' : 620
+        },
+        {
+          'input' : 'lorem ipsum dolor sit amet',
+          'output' : 'Success',
+          'expected' : 'Success',
+          'load' : 240,
+          'execution' : 320
+        },
+        {
+          'input' : 'lorem ipsum dolor sit amet',
+          'output' : 'data is required',
+          'expected' : 'Success',
+          'load' : 420,
+          'execution' : 120
+        }
+      ]
+    }
+  }
+}
