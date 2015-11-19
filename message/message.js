68eb3c7c49e433766d834ac5cf7ddcea60b1be80
kokok <sunupf@gmail.com>
2015-11-19 19:23:42 +0700

    wait element

--- message/message.js
+++ message/message.js
@@ -1,7 +1,11 @@
 var messages = {
   'messages':{
     'required': 'The :attribute field is required.',
-    'unique': 'The :attribute has already been taken.'
+    'unique': 'The :attribute has already been taken.',
+    'max': {
+  		'numeric': 'The :attribute must be less than :max.',
+  		'string': 'The :attribute may not be greater than :max characters.'
+  	}
   },
   'setMessage':function(key,msg){
     this.messages[key] = msg
