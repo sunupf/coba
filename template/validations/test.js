8348f5edc9c42506b74e5943ab298b4c7367a907
Sunu Pinasthika Fajar <sunupf@gmail.com>
2016-12-24 09:33:10 +0700

    add custom validation

---
+++ template/validations/test.js
@@ -0,0 +1,13 @@
+var testCustomRule = function(param,negation){
+  if(typeof negation != "undefined"){
+    negation = false
+  }
+
+  // you can code anything here to create generator for custom validation
+  // you can set negation to true if you want generate right value for that validation
+  // false if you don't
+
+  // you can pass anything you want into param
+}
+
+module.exports = testCustomRule;
