da69397935634bbefaf013f458a27e670ceb0794
Sunu Pinasthika Fajar <sunupf@gmail.com>
2017-01-06 16:54:37 +0700

    increase version number, change some code to compatible with ruby coba test runner

--- template/test.js
+++ template/test.js
@@ -1,3 +1,6 @@
+// Use this when you want test using JS
+// Update your config to change language you used
+
 var customTest = function(testSuite){
   var data = testSuite.data;
   var coba = testSuite.coba;
